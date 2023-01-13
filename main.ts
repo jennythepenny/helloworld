26025b2db16472bb10c38f46d7e91c212f03cb42
jennythepenny <122555254+jennythepenny@users.noreply.github.com>
2023-01-13 12:25:03 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,9 +8,21 @@ input.onButtonPressed(Button.A, function () {
     basic.showArrow(ArrowNames.West)
     basic.showArrow(ArrowNames.NorthWest)
 })
+input.onGesture(Gesture.TiltLeft, function () {
+    basic.showIcon(IconNames.Yes)
+})
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.Heart)
     basic.pause(200)
     basic.showIcon(IconNames.SmallHeart)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Confused)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    basic.showIcon(IconNames.No)
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showIcon(IconNames.Happy)
 })
 basic.showString("school :D")
