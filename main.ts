003e26fc4bd7df17b3c24cdabd642a8a07ad129e
jennythepenny <122555254+jennythepenny@users.noreply.github.com>
2023-01-13 12:06:35 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,6 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.SmallHeart)
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showIcon(IconNames.Ghost)
+    basic.showIcon(IconNames.Angry)
 })
 basic.showIcon(IconNames.Heart)
